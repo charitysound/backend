bf700c95a072ea666973cfd9329fb2852433d395
Alex Crawford <awc737@gmail.com>
2013-08-23 18:22:00 -0700

    init

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			sass: {
+				files: 'public/sass/*',
+				tasks: ['compass:dev']
+			},
+			neuter: {
+				files: ['public/js/app/**/*.js'],
+                tasks: ['neuter']
+			}
+		},
+		compass: {
+			dev: {
+				options: {
+					sassDir: 'public/sass',
+					cssDir: 'public/css',
+				}
+			},
+			dist: {
+				options: {
+					sassDir: 'public/sass',
+					cssDir: 'dist/css',
+					outputStyle: 'compressed'
+				}
+			}
+		},
+		neuter: {
+			dev: {
+                options: {
+                    filepathTransform: function (filepath) {
+                        return 'public/js/app/' + filepath;
+                    }
+                },
+				src: 'public/js/app/app.js',
+				dest: 'public/js/global.js'
+			}
+		},
+       	copy: {
+            dev: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'public/',
+                    dest: 'dist/',
+                    src: [
+                        'js/global.js'
+                    ]
+                }]
+            }
+        },
+		useminPrepare: {
+			html: 'app/views/templates/**/*.php',
+			options: {
+                dest: 'dist/'
+            }
+		},
+		usemin: {
+			html: ['app/views/templates/**/*.php'],
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-neuter');
+	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+	grunt.registerTask('default', ['compass:dev', 'neuter']);
+	grunt.registerTask('dist', ['compass:dist', 'neuter', 'copy', 'useminPrepare', 'concat', 'uglify', 'usemin' ]);
+
+};
---
+++ app/views/templates/default.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="shortcut icon" href="../../assets/ico/favicon.png">
+
+		<title>Charity Sound</title>
+
+		<link rel="stylesheet" href="{{ asset('css/global.css') }}">
+
+		<!--[if lt IE 9]>
+			<script src="{{ asset('js/libs/html5shiv.js') }}"></script>
+			<script src="{{ asset('js/libs/respond.min.js') }}"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">CharitySound</a>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Home</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Login</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			@yield('content')
+		</div>
+
+		<!-- build:js js/libs.js -->
+		<script src="{{ asset('js/libs/jquery.js') }}"></script>
+		<script src="{{ asset('js/libs/bootstrap.min.js') }}"></script>
+		<!-- endbuild -->
+
+	</body>
+</html>
